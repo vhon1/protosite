594df323df65c0e3ebd94fd9034610c9fab85439
rpoulos <rpoulos@deloitte.co.uk>
2018-03-29 10:41:06 -0400

    pretty colours

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -18,4 +18,9 @@ describe 'Prototype App' do
     get '/'
     expect(last_response.body).to include "Prototype Inc"
   end
+  
+  it 'displays the correct content' do
+    get '/'
+    expect(last_response.body).to include "Home of the World's Best Prototypes"
+  end
 end
